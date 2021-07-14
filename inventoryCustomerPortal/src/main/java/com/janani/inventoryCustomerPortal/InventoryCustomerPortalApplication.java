8b5702d982f0c721debe5d057ac26032cb0a258a
jlenagala <jananianut@gmail.com>
2021-07-15 01:15:12 +0530

    iniial commit

---
+++ inventoryCustomerPortal/src/main/java/com/janani/inventoryCustomerPortal/InventoryCustomerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.janani.inventoryCustomerPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryCustomerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryCustomerPortalApplication.class, args);
+	}
+
+}
---
+++ inventoryCustomerPortal/src/test/java/com/janani/inventoryCustomerPortal/InventoryCustomerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janani.inventoryCustomerPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryCustomerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
