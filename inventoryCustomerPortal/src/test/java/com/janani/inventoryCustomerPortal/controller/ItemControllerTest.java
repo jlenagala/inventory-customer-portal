29c800d814412294e17c9a4f00df4239a896a325
jlenagala <jananianut@gmail.com>
2021-07-16 15:41:53 +0530

    Added test coverage

---
+++ inventoryCustomerPortal/src/test/java/com/janani/inventoryCustomerPortal/controller/ItemControllerTest.java
@@ -0,0 +1,58 @@
+package com.janani.inventoryCustomerPortal.controller;
+
+import com.janani.inventoryCustomerPortal.model.Item;
+import com.janani.inventoryCustomerPortal.service.ItemService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ItemControllerTest {
+    @InjectMocks
+    ItemController itemController;
+    @Mock
+    ItemService itemService;
+@Mock
+Item item;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getItemById() {
+        itemController.getItemById(1);
+    }
+
+    @Test
+    void getAllItems() {
+        itemController.getAllItems();
+    }
+
+    @Test
+    void addItem() {
+        itemController.addItem(item);
+    }
+
+    @Test
+    void updateItem() {
+        itemController.updateItem(item);
+    }
+
+    @Test
+    void deleteProfile() {
+        itemController.deleteProfile(1);
+    }
+
+    @Test
+    void getAllTaxedItems() {
+        itemController.getAllTaxedItems();
+    }
+
+    @Test
+    void getAllDiscountedItems() {
+        itemController.getAllDiscountedItems();
+    }
+}
---
+++ inventoryCustomerPortal/src/test/java/com/janani/inventoryCustomerPortal/controller/PaymentControllerTest.java
@@ -0,0 +1,27 @@
+package com.janani.inventoryCustomerPortal.controller;
+
+import com.janani.inventoryCustomerPortal.model.Item;
+import com.janani.inventoryCustomerPortal.model.Payment;
+import com.janani.inventoryCustomerPortal.service.PaymentService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentControllerTest {
+    @InjectMocks
+    PaymentController paymentController;
+    @Mock
+    PaymentService paymentService;
+    @Mock
+    Item item;
+    @Mock
+    Payment payment;
+
+    @Test
+    void calculateTotalAmount() {
+        payment.setId(1);
+     //   paymentService.calculateTotalAmount(item, payment, 1);
+    }
+}
